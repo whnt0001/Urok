e9157488f92ea470d0374ea16e17d01d1704ffd2
whnt0001 <97937508+whnt0001@users.noreply.github.com>
2022-03-30 09:39:08 +0300

    Add files via upload

---
+++ urok2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using urok2.Page;
+
+namespace urok2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == a & password.Text == b)
+            {
+               Users users = new Users();
+               users.Show();
+            }
+            else MessageBox.Show("Неверный логин и пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        string a = "admin";
+        string b = "admin";
+
+    }
+}
